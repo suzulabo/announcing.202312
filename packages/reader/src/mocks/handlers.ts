33d363512e796fc1acccdc225eaf0b209c0d3e3f
Kenji Suzuki <41730006+suzulabo@users.noreply.github.com>
2023-12-28 12:30:09 +0900

    Add mock handlers

--- packages/reader/src/mocks/handlers.ts
+++ packages/reader/src/mocks/handlers.ts
@@ -1,14 +1,21 @@
-import { http, HttpResponse } from 'msw';
+import { delay, http, HttpResponse } from 'msw';
 
 export const handlers = [
-  http.get('https://test.mock/', () => {
+  http.get('https://test.mock/announcing.json', () => {
     return HttpResponse.json(
-      { status: 200 },
+      {},
       {
         headers: {
           'Access-Control-Allow-Origin': '*',
         },
       },
     );
+  }),
+  http.get('https://test.mock/nocors.json', () => {
+    return HttpResponse.json({});
+  }),
+  http.get('https://test.mock/timeout.json', async () => {
+    await delay(11 * 1000);
+    return HttpResponse.json({});
   }),
 ];
