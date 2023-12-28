423cb4bbb04144d3c60f0e3e4a39ca8edc2cf123
Kenji Suzuki <41730006+suzulabo@users.noreply.github.com>
2023-12-28 12:29:15 +0900

    Fix timeout

--- packages/reader/src/lib/fetcher.ts
+++ packages/reader/src/lib/fetcher.ts
@@ -3,13 +3,13 @@ type FetchFunction = typeof fetch;
 export class Fetcher {
   constructor(private fetch: FetchFunction) {}
 
-  get(url: string) {
+  async get(url: string) {
     const controller = new AbortController();
     const timeout = setTimeout(() => {
       controller.abort();
     }, 10 * 1000);
     try {
-      return this.fetch(url);
+      return await this.fetch(url, { signal: controller.signal });
     } finally {
       clearTimeout(timeout);
     }
