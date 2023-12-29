b6019a0a6854dad8bc9c326b82025504001d9a5b
Kenji Suzuki <41730006+suzulabo@users.noreply.github.com>
2023-12-29 15:05:50 +0900

    setup eslint

--- packages/json/.eslintrc.cjs
+++ packages/json/.eslintrc.cjs
@@ -1,4 +1,14 @@
-/** @type {import('eslint').Linter.Config} */
+/** @type { import("eslint").Linter.FlatConfig } */
 module.exports = {
   root: true,
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  env: {
+    node: true,
+  },
 };
