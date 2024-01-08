da6f31350c918bfe117fec26be9ca922af9575b1
Kenji Suzuki <41730006+suzulabo@users.noreply.github.com>
2024-01-08 17:31:29 +0900

    Make info design (float version)

--- packages/reader/src/mocks/handlers.ts
+++ packages/reader/src/mocks/handlers.ts
@@ -3,7 +3,45 @@ import { delay, http, HttpResponse } from 'msw';
 export const handlers = [
   http.get('https://test.mock/announcing.json', () => {
     return HttpResponse.json(
-      {},
+      {
+        updated: '2024-01-01T22:33:44',
+        info: {
+          name: 'ヘブンバーンズレッド公式',
+          desc: 'ライトフライヤースタジオ× Keyが贈る、麻枝 准 15年ぶりの完全新作ゲーム『ヘブンバーンズレッド（ヘブバン）』の公式Twitterです。App Store/Google Play/Steamで配信中',
+          icon: 'https://pbs.twimg.com/profile_images/1664453907198906369/BQWJVvKg_400x400.jpg',
+          link: 'https://heaven-burns-red.com/',
+        },
+        posts: [
+          {
+            id: '1',
+            published: '2024-01-01T22:33:44',
+            title: 'post1',
+          },
+          {
+            id: '2',
+            published: '2024-01-01T22:33:44',
+            body: 'post2',
+          },
+        ],
+      },
+      {
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+        },
+      },
+    );
+  }),
+  http.get('https://test.mock/en/announcing.json', () => {
+    return HttpResponse.json(
+      {
+        updated: '2024-01-01T22:33:44',
+        info: {
+          name: 'DISSIDIA FINAL FANTASY OPERA OMNIA',
+          desc: 'Welcome to the official DISSIDIA FINAL FANTASY OPERA OMNIA Twitter page!\nGoogle Play: http://sqex.to/dffoo_android\nApp Store: http://sqex.to/dffoo_ios',
+          icon: 'https://pbs.twimg.com/profile_images/1651664225276928000/kTmkcbpj_400x400.jpg',
+        },
+        posts: [],
+      },
       {
         headers: {
           'Access-Control-Allow-Origin': '*',
--- packages/reader/src/routes/[...url]/+page.svelte
+++ packages/reader/src/routes/[...url]/+page.svelte
@@ -4,15 +4,66 @@
   export let data: PageData;
 </script>
 
-{#if data.error}
-  {#if data.error === 'RESPONSE'}
-    RESPONSE ERROR
+<div class="main">
+  {#if data.error}
+    {#if data.error === 'RESPONSE'}
+      RESPONSE ERROR
+    {/if}
+    {#if data.error === 'JSON'}
+      JSON ERROR
+    {/if}
   {/if}
-  {#if data.error === 'JSON'}
-    JSON ERROR
-  {/if}
-{/if}
 
-{#if data.json}
-  <div>{data.json.info.name}</div>
-{/if}
+  {#if data.json}
+    {@const { info } = data.json}
+    <div class="info">
+      {#if info.icon}
+        <img class="icon" src={info.icon} alt={info.name} />
+      {/if}
+      <div>
+        <div class="name">
+          {info.name || ''}
+        </div>
+        <div class="desc">
+          {info.desc || ''}
+        </div>
+        {#if info.link}
+          <div class="link">
+            <a href={info.link} target="_blank" rel="nofollow noreferrer">{info.link}</a>
+          </div>
+        {/if}
+      </div>
+    </div>
+  {/if}
+</div>
+
+<style lang="scss">
+  .main {
+    max-width: 600px;
+    margin: 0 auto;
+    padding: 20px 10px;
+
+    .info {
+      .icon {
+        float: right;
+        width: 64px;
+        height: 64px;
+        border-radius: 8px;
+        object-fit: contain;
+        margin: 0 0 0 5px;
+      }
+      .name {
+        font-weight: bold;
+        font-size: 24px;
+        margin: 0 0 10px 0;
+      }
+      .desc {
+        white-space: pre-line;
+      }
+      .link {
+        margin: 10px 5px 0 5px;
+        text-decoration: underline;
+      }
+    }
+  }
+</style>
