f305ea8c5d21f6770877df0a144e6131af78b113
Kenji Suzuki <41730006+suzulabo@users.noreply.github.com>
2024-02-03 19:11:34 +0900

    Design sign in page(WIP)

--- packages/writer/src/routes/signin/+page.svelte
+++ packages/writer/src/routes/signin/+page.svelte
@@ -5,21 +5,38 @@
   import { t } from '$lib/i18n/translations';
 </script>
 
-<Button
-  on:click={() => {
-    signIn('google');
-  }}
-  ><div class="sign-in-button-inner">
-    {@html googleIcon}<span class="label">{$t('signIn', { placeholder: 'Google' })}</span>
-  </div></Button
->
+<div class="main">
+  <div class="buttons">
+    <Button
+      on:click={() => {
+        signIn('google');
+      }}
+      ><div class="inner">
+        {@html googleIcon}<span class="label">{$t('signIn', { placeholder: 'Google' })}</span>
+      </div></Button
+    >
+  </div>
+</div>
 
 <style lang="scss">
-  .sign-in-button-inner {
-    display: inline-flex;
-    align-items: center;
-    .label {
-      margin-left: 10px;
+  .main {
+    max-width: 600px;
+    margin: 0 auto;
+    padding: 20px 10px;
+
+    .buttons {
+      max-width: 300px;
+      display: flex;
+      margin: 0 auto;
+      flex-direction: column;
+
+      .inner {
+        display: inline-flex;
+        align-items: center;
+        .label {
+          margin-left: 10px;
+        }
+      }
     }
   }
 </style>
