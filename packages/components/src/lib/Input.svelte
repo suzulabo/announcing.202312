89a56c7e3a82e446735cc27050e07ff7a97d5745
Kenji Suzuki <41730006+suzulabo@users.noreply.github.com>
2024-03-21 16:15:22 +0900

    Add Input

---
+++ packages/components/src/lib/Input.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  export let label: string;
+  export let type = 'text';
+  export let value = '';
+</script>
+
+<label
+  ><span>{label}</span>
+  <input {type} {value} />
+</label>
+
+<style lang="scss">
+  input {
+    margin-top: 4px;
+  }
+</style>
--- packages/components/src/routes/+page.svelte
+++ packages/components/src/routes/+page.svelte
@@ -1,18 +1,32 @@
 <script>
+  import Input from '$lib/Input.svelte';
   import Logo from '$lib/Logo.svelte';
 </script>
 
-<button
-  on:click={() => {
-    alert('click');
-  }}>Button</button
->
-<hr />
-<a class="button" href="/to">Anchor Button</a>
-<hr />
-<Logo size="100px" />
+<div class="main">
+  <button
+    on:click={() => {
+      alert('click');
+    }}>Button</button
+  >
+
+  <hr />
+  <a class="button" href="/to">Anchor Button</a>
+
+  <hr />
+  <Logo size="100px" />
+
+  <hr />
+  <Input label="Label" />
+
+  <hr />
+  <div class="info">Information Panel</div>
+</div>
 
 <style lang="scss">
+  .main {
+    padding: 16px;
+  }
   hr {
     margin: 10px 0;
   }
