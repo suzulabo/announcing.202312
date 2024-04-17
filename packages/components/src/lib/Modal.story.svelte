85c51a3969bb686aa95103b742b907f3ad5a4e47
Kenji Suzuki <41730006+suzulabo@users.noreply.github.com>
2024-04-17 16:27:37 +0900

    Add Modal

---
+++ packages/components/src/lib/Modal.story.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import type { Hst } from '@histoire/plugin-svelte';
+  import Modal from './Modal.svelte';
+
+  // eslint-disable-next-line no-import-assign
+  export let Hst: Hst;
+
+  let show = false;
+</script>
+
+<Hst.Story title="Modal">
+  <button on:click={() => (show = true)}>Show</button>
+  <Modal bind:show>
+    <div class="modal-body">
+      <div>This is a modal window.</div>
+      <button on:click={() => (show = false)}>Close</button>
+    </div>
+  </Modal>
+</Hst.Story>
+
+<style lang="scss">
+  .modal-body {
+    padding: 8px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 16px;
+  }
+</style>
---
+++ packages/components/src/lib/Modal.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+  export let show = false;
+</script>
+
+{#if show}
+  <!-- svelte-ignore a11y-click-events-have-key-events  -->
+  <!-- svelte-ignore a11y-no-static-element-interactions -->
+  <div class="modal" on:click|self={() => (show = false)}>
+    <div class="modal-content"><slot /></div>
+  </div>
+{/if}
+
+<svelte:window
+  on:keydown={(event) => {
+    if (event.key === 'Escape') {
+      show = false;
+    }
+  }}
+/>
+
+<style lang="scss">
+  .modal {
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    margin: 0;
+    background-color: var(--color-background-modal);
+    backdrop-filter: blur(1px);
+    z-index: var(--modal-z-index, 999);
+
+    padding: 8px;
+    display: flex;
+
+    .modal-content {
+      background-color: var(--color-background);
+      border-radius: 4px;
+      margin: auto;
+    }
+  }
+</style>
