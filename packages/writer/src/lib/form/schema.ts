569609d85932f2f9d7724095cf8d9c8b9d1db7af
Kenji Suzuki <41730006+suzulabo@users.noreply.github.com>
2024-05-28 11:20:52 +0900

    maxSize in icon

--- packages/writer/src/lib/form/schema.ts
+++ packages/writer/src/lib/form/schema.ts
@@ -1,8 +1,8 @@
 import { THREAD_DESC_MAX_LENGTH, THREAD_TITLE_MAX_LENGTH } from '$lib/constants';
-import { instance, maxLength, minLength, object, optional, string } from 'valibot';
+import { instance, maxLength, maxSize, minLength, object, optional, string } from 'valibot';
 
 export const create = object({
-  icon: optional(instance(File)),
+  icon: optional(instance(File, [maxSize(1024 * 1024)])),
   title: string([minLength(1), maxLength(THREAD_TITLE_MAX_LENGTH)]),
   desc: optional(string([maxLength(THREAD_DESC_MAX_LENGTH)])),
 });
