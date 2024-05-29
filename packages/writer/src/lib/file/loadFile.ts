6842f624691fbd5d3b883cdb82c2f006e4ef00da
Kenji Suzuki <41730006+suzulabo@users.noreply.github.com>
2024-05-29 10:45:57 +0900

    Add storage API

---
+++ packages/writer/src/lib/file/loadFile.ts
@@ -0,0 +1,31 @@
+import { createReadableStream } from '@sveltejs/kit/node';
+import { readFile, stat } from 'fs/promises';
+
+const loadFileLocal = async (hash: string) => {
+  try {
+    const [metaBuf, stats] = await Promise.all([
+      readFile(`storage/${hash}.meta`),
+      stat(`storage/${hash}`),
+    ]);
+
+    const meta = JSON.parse(metaBuf.toString());
+
+    const contentType = meta['type'] as string;
+
+    const contentLength = stats.size;
+
+    const content = createReadableStream(`storage/${hash}`);
+
+    return { contentType, contentLength, content } as const;
+  } catch (err) {
+    if (err instanceof Error && 'code' in err && err.code === 'ENOENT') {
+      return;
+    }
+
+    throw err;
+  }
+};
+
+const loadFile = loadFileLocal;
+
+export default loadFile;
