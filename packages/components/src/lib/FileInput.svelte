c94a56daea2c5c0cdaa86ba90760857721528a90
Kenji Suzuki <41730006+suzulabo@users.noreply.github.com>
2024-06-13 13:37:38 +0900

    fix

--- packages/components/src/lib/FileInput.svelte
+++ packages/components/src/lib/FileInput.svelte
@@ -5,24 +5,25 @@
   export let name: string;
   export let file: File | null | undefined = undefined;
   export let files: File[] | null | undefined = undefined;
-  export let removed: boolean = false;
   export let accept: string | undefined = undefined;
   export let maxImageSize: number | undefined = undefined;
   export let filesCount: number = 1;
 
   let fileInput: HTMLInputElement;
   let valueInput: HTMLInputElement;
-  let removedInput: HTMLInputElement;
   let loading = false;
+
+  $: syncInputFiles = () => {
+    const a = file ? [file] : files ?? [];
+
+    const dt = new DataTransfer();
+
+    a.forEach((v) => dt.items.add(v));
+    valueInput.files = dt.files;
+  };
 
   export const open = () => {
     fileInput.click();
-  };
-  export const remove = () => {
-    removedInput.value = 'true';
-    removed = true;
-    file = undefined;
-    files = undefined;
   };
 
   const fileInputChange = async () => {
@@ -60,19 +61,14 @@
         newFiles.push(new File([reduced], f.name, { type: reduced.type }));
       }
 
-      const dt = new DataTransfer();
-
-      newFiles.forEach((v) => dt.items.add(v));
-      valueInput.files = dt.files;
-
       if (filesCount > 1) {
         files = newFiles;
       } else {
         file = newFiles[0];
       }
 
+      syncInputFiles();
       fileInput.value = '';
-      removed = false;
     } finally {
       loading = false;
     }
@@ -80,12 +76,6 @@
 </script>
 
 <input type="file" style="display:none" {name} bind:this={valueInput} multiple={filesCount > 1} />
-<input
-  type="hidden"
-  name={`${name}Removed`}
-  bind:this={removedInput}
-  value={removed ? 'true' : ''}
-/>
 
 <input
   type="file"
