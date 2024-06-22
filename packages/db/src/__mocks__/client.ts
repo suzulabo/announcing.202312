7ad027dfa052d4c30f58a3838c0fefbd3c173ef3
Kenji Suzuki <41730006+suzulabo@users.noreply.github.com>
2024-06-22 18:00:22 +0900

    Setup test

---
+++ packages/db/src/__mocks__/client.ts
@@ -0,0 +1,10 @@
+import { createClient } from '@libsql/client';
+import { drizzle } from 'drizzle-orm/libsql';
+import { migrate } from 'drizzle-orm/libsql/migrator';
+import * as schema from '../schema';
+
+const sqlite = createClient({ url: ':memory:' });
+
+export const db = drizzle(sqlite, { logger: true, schema });
+
+await migrate(db, { migrationsFolder: './drizzle' });
