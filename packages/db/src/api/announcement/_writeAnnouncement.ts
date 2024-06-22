f10629f69183735cc48db4aac135743624e7587a
Kenji Suzuki <41730006+suzulabo@users.noreply.github.com>
2024-06-22 16:33:06 +0900

    announcement API

---
+++ packages/db/src/api/announcement/_writeAnnouncement.ts
@@ -0,0 +1,92 @@
+import { and, eq, sql } from 'drizzle-orm';
+import { db } from '../../client';
+import { genHash } from '../../lib/genHash';
+import { storeFile } from '../../lib/storeFile';
+import { stripNullish } from '../../lib/stripNullish';
+import { channelsTable, type Announcement, type Announcements } from '../../schema';
+
+export const _writeAnnouncement = async (
+  userID: string,
+  channelID: string,
+  channelUpdatedAt: number,
+  headerImageFile: File | undefined | null,
+  title: string | undefined | null,
+  body: string,
+  imagesFiles: File[] | undefined | null,
+  updateAnnouncementId: string | undefined,
+) => {
+  const updatedAtDate = new Date(channelUpdatedAt);
+
+  const channel = (
+    await db
+      .select({ announcements: channelsTable.announcements })
+      .from(channelsTable)
+      .where(
+        and(
+          eq(channelsTable.channelID, channelID),
+          eq(channelsTable.updatedAt, updatedAtDate),
+          sql`EXISTS(SELECT 1 FROM json_each(owners) WHERE value = ${userID})`,
+        ),
+      )
+  ).shift();
+
+  if (!channel) {
+    return;
+  }
+
+  const headerImage = (headerImageFile && (await storeFile(headerImageFile))) || undefined;
+
+  const images =
+    imagesFiles && imagesFiles.length > 0
+      ? await Promise.all(imagesFiles.map((v) => storeFile(v)))
+      : undefined;
+
+  const nowDate = new Date();
+
+  const now = nowDate.getTime();
+
+  const id = genHash([`${now}`, body, title, headerImage, ...(images ?? [])]);
+
+  const announcements: Announcements = channel.announcements || [];
+
+  if (!updateAnnouncementId) {
+    const v = stripNullish<Announcement>({
+      id,
+      headerImage,
+      title,
+      body,
+      images,
+      updatedAt: now,
+      createdAt: now,
+    });
+
+    announcements.push(v);
+  } else {
+    const index = announcements.findIndex((v) => v.id === updateAnnouncementId);
+
+    if (index >= 0) {
+      const cur = announcements[index]!;
+
+      announcements[index] = stripNullish<Announcement>({
+        id,
+        headerImage,
+        title,
+        body,
+        images,
+        updatedAt: now,
+        createdAt: cur.createdAt,
+      });
+    }
+  }
+
+  await db
+    .update(channelsTable)
+    .set({ announcements, updatedAt: nowDate })
+    .where(
+      and(
+        eq(channelsTable.channelID, channelID),
+        eq(channelsTable.updatedAt, updatedAtDate),
+        sql`EXISTS(SELECT 1 FROM json_each(owners) WHERE value = ${userID})`,
+      ),
+    );
+};
---
+++ packages/db/src/api/announcement/removeAnnouncement.ts
@@ -0,0 +1,55 @@
+import { and, eq, sql } from 'drizzle-orm';
+import { db } from '../../client';
+import { channelsTable } from '../../schema';
+
+export const removeAnnouncement = async (
+  userID: string,
+  channelID: string,
+  channelUpdatedAt: number,
+  announcementID: string,
+) => {
+  const updatedAtDate = new Date(channelUpdatedAt);
+
+  const channel = (
+    await db
+      .select({ announcements: channelsTable.announcements })
+      .from(channelsTable)
+      .where(
+        and(
+          eq(channelsTable.channelID, channelID),
+          eq(channelsTable.updatedAt, updatedAtDate),
+          sql`EXISTS(SELECT 1 FROM json_each(owners) WHERE value = ${userID})`,
+        ),
+      )
+  ).shift();
+
+  if (!channel) {
+    return;
+  }
+
+  const nowDate = new Date();
+
+  const announcements = channel.announcements;
+
+  if (!announcements) {
+    return;
+  }
+
+  const index = announcements.findIndex((v) => v.id === announcementID);
+
+  if (index < 0) {
+    return;
+  }
+
+  announcements.splice(index);
+  await db
+    .update(channelsTable)
+    .set({ announcements, updatedAt: nowDate })
+    .where(
+      and(
+        eq(channelsTable.channelID, channelID),
+        eq(channelsTable.updatedAt, updatedAtDate),
+        sql`EXISTS(SELECT 1 FROM json_each(owners) WHERE value = ${userID})`,
+      ),
+    );
+};
