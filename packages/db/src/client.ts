8899a747da7afba410361dae55cb8c7f3ad269be
Kenji Suzuki <41730006+suzulabo@users.noreply.github.com>
2024-07-01 14:23:23 +0900

    Change dev.db path

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@libsql/client';
 import { drizzle } from 'drizzle-orm/libsql';
 
-const sqlite = createClient({ url: 'file:dev.db' });
+const sqlite = createClient({ url: 'file:../dev.db' });
 
 export const db = drizzle(sqlite, { logger: true });
