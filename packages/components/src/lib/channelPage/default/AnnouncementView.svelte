e7d8be4f603b6a40a5c5c6f68780c548d8103323
Kenji Suzuki <41730006+suzulabo@users.noreply.github.com>
2024-07-11 17:31:30 +0900

    Images

--- packages/components/src/lib/channelPage/default/AnnouncementView.svelte
+++ packages/components/src/lib/channelPage/default/AnnouncementView.svelte
@@ -22,11 +22,23 @@
 
 <div class="announcement">
   <div class="published">{formatDate(announcement.updatedAt)}</div>
+  {#if announcement.headerImage}
+    <div class="header-image">
+      <img src={announcement.headerImage} alt={announcement.title} />
+    </div>
+  {/if}
   {#if announcement.title}
     <div class="title">{announcement.title}</div>
   {/if}
   {#if announcement.body}
     <div class="body">{@html toHtml(announcement.body)}</div>
+  {/if}
+  {#if announcement.images}
+    <div class={`images size-${announcement.images.length}`}>
+      {#each announcement.images as image}
+        <img src={image} alt={announcement.title} />
+      {/each}
+    </div>
   {/if}
 </div>
 <hr />
@@ -35,14 +47,44 @@
   .announcement {
     padding: 0 10px;
     min-height: 100px;
+    .header-image {
+      margin: 16px 0 0;
+      text-align: center;
+      img {
+        border-radius: 4px;
+        max-height: 200px;
+      }
+    }
     .title {
       font-size: 18px;
       font-weight: bold;
-      margin: 10px 0 0;
+      margin: 16px 0 0;
     }
     .body {
       white-space: pre-line;
-      margin: 10px 0 0;
+      margin: 16px 0 0;
+    }
+    .images {
+      margin: 16px 0 0;
+      display: grid;
+      gap: 4px;
+      grid-template-columns: repeat(auto-fit, minmax(170px, 1fr));
+      img {
+        height: 170px;
+        width: 100%;
+        object-fit: cover;
+        border-radius: 4px;
+      }
+
+      &.size-1 {
+        img {
+          margin: auto;
+          width: auto;
+          height: auto;
+          max-height: 200px;
+          object-fit: contain;
+        }
+      }
     }
   }
 
