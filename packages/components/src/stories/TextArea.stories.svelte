a49cd8e777a07a8e2e8bdf7a9c8229869921d6c7
Kenji Suzuki <41730006+suzulabo@users.noreply.github.com>
2024-07-16 16:42:55 +0900

    Add TextArea stories

---
+++ packages/components/src/stories/TextArea.stories.svelte
@@ -0,0 +1,42 @@
+<script lang="ts" context="module">
+  import { Story } from '@storybook/addon-svelte-csf';
+  import type { Meta } from '@storybook/svelte';
+
+  import TextArea from '$lib/TextArea.svelte';
+
+  export const meta = {
+    title: 'TextArea',
+    component: TextArea,
+    tags: ['autodocs'],
+  } satisfies Meta<TextArea>;
+</script>
+
+<script lang="ts">
+</script>
+
+<Story name="Basic">
+  <div class="container">
+    <TextArea name="name" label="Text area" placeholder="Enter up to 500 characters" maxLength={500}
+    ></TextArea>
+  </div>
+</Story>
+
+<Story name="Long value">
+  <div class="container">
+    <TextArea
+      name="long-value"
+      label="Long value"
+      value="In the beginning God created the heavens and the earth. Now the earth was formless and empty,
+  darkness was over the surface of the deep, and the Spirit of God was hovering over the waters.
+  And God said, 'Let there be light,' and there was light. God saw that the light was good, and he
+  separated the light from the darkness. God called the light 'day,' and the darkness he called
+  'night."
+    ></TextArea>
+  </div>
+</Story>
+
+<style lang="scss">
+  .container {
+    max-width: 400px;
+  }
+</style>
