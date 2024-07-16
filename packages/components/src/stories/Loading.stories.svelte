1a30e533c45f22165160e086a020fb20c94080e5
Kenji Suzuki <41730006+suzulabo@users.noreply.github.com>
2024-07-16 16:19:36 +0900

    Add Loading stories

---
+++ packages/components/src/stories/Loading.stories.svelte
@@ -0,0 +1,16 @@
+<script lang="ts" context="module">
+  import { Story } from '@storybook/addon-svelte-csf';
+  import type { Meta } from '@storybook/svelte';
+
+  import Loading from '$lib/Loading.svelte';
+
+  export const meta = {
+    title: 'Loading',
+    component: Loading,
+    tags: ['autodocs'],
+  } satisfies Meta<Loading>;
+</script>
+
+<Story name="Basic">
+  <Loading show={true} />
+</Story>
