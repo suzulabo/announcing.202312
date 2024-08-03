71ce5ff029ba6ed62d29b789e1d781b58f42cb25
Kenji Suzuki <41730006+suzulabo@users.noreply.github.com>
2024-08-03 11:23:16 +0900

    Add dark mode stories

--- packages/components/src/lib/ChannelView/default_dark/ChannelView.svelte
+++ packages/components/src/lib/ChannelView/default_dark/ChannelView.svelte
@@ -1,20 +1,14 @@
 <script lang="ts">
   import { onMount } from 'svelte';
 
-  import ChannelView from '../default/ChannelView.svelte';
+  import DefaultChannelView from '../default/ChannelView.svelte';
   import type { ChannelViewParams } from '../types';
 
   export let params: ChannelViewParams;
 
   onMount(() => {
-    console.log('mount');
+    document.documentElement.classList.add('dark');
   });
 </script>
 
-<svelte:window
-  on:load={() => {
-    console.log('load');
-  }}
-/>
-
-<ChannelView {params} />
+<DefaultChannelView {params}><slot /></DefaultChannelView>
--- packages/components/src/stories/ChannelPage.stories.svelte
+++ packages/components/src/stories/ChannelPage.stories.svelte
@@ -4,24 +4,22 @@
   import { Story } from '@storybook/addon-svelte-csf';
   import type { Meta } from '@storybook/svelte';
 
-  import type { ChannelPageParams } from '$lib/ChannelPage';
-
   import { params } from './ChannelPage';
 
   export const meta = {
     title: 'ChannelPage/default',
     tags: ['autodocs'],
   } satisfies Meta;
-
-  const noAnnouncementsParams = (): ChannelPageParams => {
-    return { ...params, segments: [] };
-  };
 </script>
 
 <Story name="Basic">
   <channel-page {params} />
 </Story>
 
+<Story name="Dark">
+  <channel-page params={{ ...params, viewName: 'default_dark' }} />
+</Story>
+
 <Story name="No announcements">
-  <channel-page params={noAnnouncementsParams()} />
+  <channel-page params={{ ...params, segments: [] }} />
 </Story>
