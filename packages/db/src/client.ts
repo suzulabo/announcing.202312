d4e97ba62287746c75f69572dd5e43133d291104
Kenji Suzuki <41730006+suzulabo@users.noreply.github.com>
2024-08-08 18:28:42 +0900

    Change logger to be disabled

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -3,4 +3,4 @@ import { drizzle } from 'drizzle-orm/libsql';
 
 const sqlite = createClient({ url: 'file:../db-dev/dev.db' });
 
-export const db = drizzle(sqlite, { logger: true });
+export const db = drizzle(sqlite, { logger: false });
