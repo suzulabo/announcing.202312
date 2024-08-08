d383f86feb98524af2a3075c1fa2f547dc3b37d7
Kenji Suzuki <41730006+suzulabo@users.noreply.github.com>
2024-08-08 14:11:56 +0900

    Comment out workers setting

--- packages/components/playwright.config.ts
+++ packages/components/playwright.config.ts
@@ -18,8 +18,12 @@ export default defineConfig({
   forbidOnly: !!process.env['CI'],
   /* Retry on CI only */
   retries: process.env['CI'] ? 2 : 0,
+
   /* Opt out of parallel tests on CI. */
-  ...(process.env['CI'] ? { workers: 2 } : {}),
+  // ### Github runner is not poor.
+  // https://docs.github.com/en/actions/using-github-hosted-runners/about-github-hosted-runners/about-github-hosted-runners#standard-github-hosted-runners-for-public-repositories
+  // ...(process.env['CI'] ? { workers: 2 } : {}),
+
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
