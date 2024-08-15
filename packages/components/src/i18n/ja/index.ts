c51e66885f0a5aa25ccf0a4e86b35c63137a8c23
Kenji Suzuki <41730006+suzulabo@users.noreply.github.com>
2024-08-15 16:07:18 +0900

    Change Input to check maxBytes

--- packages/components/src/i18n/ja/index.ts
+++ packages/components/src/i18n/ja/index.ts
@@ -1,8 +1,7 @@
-import type { Translation } from '../i18n-types'
+import type { Translation } from '../i18n-types';
 
-const de = {
-	// this is an example Translation, just rename or delete this folder if you want
-	HI: 'Hallo {name}! Bitte hinterlasse einen Stern, wenn dir das Projekt gefällt: https://github.com/ivanhofer/typesafe-i18n',
-} satisfies Translation
+const ja = {
+  textTooLong: '入力が文字数制限を超えています',
+} satisfies Translation;
 
-export default de
+export default ja;
