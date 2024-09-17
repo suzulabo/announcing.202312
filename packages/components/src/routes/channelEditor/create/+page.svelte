a9ae2c535f4ad653f25b2e37c3656ce0e8ed4826
Kenji Suzuki <41730006+suzulabo@users.noreply.github.com>
2024-09-17 17:36:11 +0900

    Remove default slot

--- packages/components/src/routes/channelEditor/create/+page.svelte
+++ packages/components/src/routes/channelEditor/create/+page.svelte
@@ -16,31 +16,11 @@
   on:submit={({ detail }) => {
     console.log(detail);
   }}
->
-  <div class="modal-body">
-    <div>This is a modal window.</div>
-    <button
-      on:click={() => {
-        editor.closeModal();
-      }}>Close</button
-    >
-  </div>
-</ChannelEditor>
+/>
 
 <style lang="scss">
   .container {
     text-align: center;
     padding: 30vh;
-  }
-
-  .modal-body {
-    background-color: var(--color-background);
-    border-radius: 8px;
-    margin: auto;
-    padding: 8px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: 16px;
   }
 </style>
