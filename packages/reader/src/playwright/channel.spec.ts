4292de1b2d7da7917b8fb818b3aea4793bf78acc
Kenji Suzuki <41730006+suzulabo@users.noreply.github.com>
2024-09-19 15:46:14 +0900

    Disable playwright in reader

--- packages/reader/src/playwright/channel.spec.ts
+++
@@ -1,8 +0,0 @@
-import test, { expect } from '@playwright/test';
-
-test('channel-page', async ({ page }) => {
-  await page.goto('/9901');
-  await page.locator('.main').waitFor();
-
-  await expect(page).toHaveScreenshot({ fullPage: true });
-});
