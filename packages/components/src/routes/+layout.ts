9aa379d761dfc24e898059ef678594453f10a5b2
Kenji Suzuki <41730006+suzulabo@users.noreply.github.com>
2024-10-15 11:26:48 +0900

    Fix build error

--- packages/components/src/routes/+layout.ts
+++ packages/components/src/routes/+layout.ts
@@ -1,5 +1,8 @@
 import '$lib/base.scss';
 
 import { setupLocale } from '@announcing/i18n';
+import type { Load } from '@sveltejs/kit';
 
-await setupLocale('ja');
+export const load: Load = async () => {
+  await setupLocale('ja');
+};
