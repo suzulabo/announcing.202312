535d814f9346afb6136244f9637e7b2d86506190
Kenji Suzuki <41730006+suzulabo@users.noreply.github.com>
2024-10-18 12:34:44 +0900

    Add button catalogue

---
+++ packages/components/src/routes/atoms/button/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { LL } from '@announcing/i18n';
+</script>
+
+<div class="container">
+  <button>{$LL.createChannel()}</button>
+  <button disabled>{$LL.createChannel()}</button>
+</div>
+
+<style lang="scss">
+  .container {
+    display: flex;
+    gap: 16px;
+    width: fit-content;
+    margin: 30vh auto;
+  }
+</style>
