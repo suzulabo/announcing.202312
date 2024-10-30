348c142d7983948b01f11ad9d0e58be04b465df1
Kenji Suzuki <41730006+suzulabo@users.noreply.github.com>
2024-10-30 10:29:37 +0900

    Update button style. Add highlight background color.

--- packages/components/src/routes/atoms/button/+page.svelte
+++ packages/components/src/routes/atoms/button/+page.svelte
@@ -7,6 +7,8 @@
   <button disabled>{$LL.createChannel()}</button>
   <button class="text">{$LL.createChannel()}</button>
   <button class="text" disabled>{$LL.createChannel()}</button>
+  <button class="highlight">{$LL.createChannel()}</button>
+  <button class="highlight" disabled>{$LL.createChannel()}</button>
   <a class="button" href="https://github.com">Go to Github</a>
 </div>
 
