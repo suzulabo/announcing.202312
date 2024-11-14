6288832006bc0e50e96b638122ff2fc52277ce8d
Kenji Suzuki <41730006+suzulabo@users.noreply.github.com>
2024-11-14 12:32:13 +0900

    clean scripts in package.json of db

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -3,7 +3,4 @@ import { defineConfig } from 'drizzle-kit';
 export default defineConfig({
   dialect: 'sqlite',
   schema: './src/schema.ts',
-  dbCredentials: {
-    url: 'file:../db-dev/dev.db',
-  },
 });
