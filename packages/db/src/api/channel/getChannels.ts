e46223672dedf00f36a632fa574d3038b3daf44c
Kenji Suzuki <41730006+suzulabo@users.noreply.github.com>
2024-11-18 12:56:32 +0900

    Add performance measurement to getChannels

--- packages/db/src/api/channel/getChannels.ts
+++ packages/db/src/api/channel/getChannels.ts
@@ -6,6 +6,8 @@ import { getDB } from '../db';
 export const getChannels = async ({ userID }: { userID: string }) => {
   const db = getDB();
 
+  const start = performance.now();
+
   const channels = await db
     .select({
       channelID: channelsTable.channelID,
@@ -19,6 +21,10 @@ export const getChannels = async ({ userID }: { userID: string }) => {
     .from(channelsTable)
     .innerJoin(ownersTable, eq(channelsTable.channelID, ownersTable.channelID))
     .where(eq(ownersTable.userID, userID));
+
+  const end = performance.now();
+  const duration = end - start;
+  console.log(`[PERF-DB] getChannels - ${duration.toFixed(2)} ms`);
 
   return channels.map((channel) => {
     return {
