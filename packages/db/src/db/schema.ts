7e6c16acf3298175319a330905a4f1342c9a3457
Kenji Suzuki <41730006+suzulabo@users.noreply.github.com>
2025-03-16 12:06:51 +0900

    Add check constraints to database

--- packages/db/src/db/schema.ts
+++ packages/db/src/db/schema.ts
@@ -1,14 +1,53 @@
-import { blob, integer, primaryKey, sqliteTable, text } from 'drizzle-orm/sqlite-core';
+import { sql } from 'drizzle-orm';
+import { check, integer, primaryKey, sqliteTable, text } from 'drizzle-orm/sqlite-core';
+import {
+  ANNOUNCEMENT_BODY_MAX_BYTES,
+  ANNOUNCEMENT_ID_LENGTH,
+  ANNOUNCEMENT_TITLE_MAX_BYTES,
+  CHANNEL_DESC_MAX_BYTES,
+  CHANNEL_ICON_MAX_BYTES,
+  CHANNEL_ID_MAX_BYTES,
+  CHANNEL_NAME_MAX_BYTES,
+  STORAGE_ID_MAX_BYTES,
+  USER_ID_MAX_BYTES,
+} from '../lib/constants';
 
-export const channelsTable = sqliteTable('channels', {
-  channelID: text('channelID').notNull().primaryKey(),
-  name: text('name').notNull(),
-  desc: text('desc'),
-  icon: text('icon'),
-  announcementIDs: text('announcementIDs', { mode: 'json' }).$type<string[]>(),
-  updatedAt: integer('updatedAt').notNull(),
-  createdAt: integer('createdAt').notNull(),
-});
+export const channelsTable = sqliteTable(
+  'channels',
+  {
+    channelID: text('channelID').notNull().primaryKey(),
+    name: text('name').notNull(),
+    desc: text('desc'),
+    icon: text('icon'),
+    announcementIDs: text('announcementIDs', { mode: 'json' }).$type<string[]>(),
+    updatedAt: integer('updatedAt').notNull(),
+    createdAt: integer('createdAt').notNull(),
+  },
+  (table) => {
+    return [
+      check(
+        'channelID',
+        sql`octet_length(${table.channelID}) BETWEEN 1 AND ${CHANNEL_ID_MAX_BYTES}`.inlineParams(),
+      ),
+      check(
+        'name',
+        sql`octet_length(${table.name}) BETWEEN 1 AND ${CHANNEL_NAME_MAX_BYTES}`.inlineParams(),
+      ),
+      check(
+        'desc',
+        sql`${table.desc} IS NULL OR octet_length(${table.desc}) <= ${CHANNEL_DESC_MAX_BYTES}`.inlineParams(),
+      ),
+      check(
+        'icon',
+        sql`${table.icon} IS NULL OR octet_length(${table.icon}) <= ${CHANNEL_ICON_MAX_BYTES}`.inlineParams(),
+      ),
+      check(
+        'announcementIDs',
+        sql`${table.announcementIDs} IS NULL OR json_type(${table.announcementIDs}) = 'array'`.inlineParams(),
+      ),
+    ];
+  },
+);
 
 export const ownersTable = sqliteTable(
   'owners',
@@ -20,7 +59,17 @@ export const ownersTable = sqliteTable(
     createdAt: integer('createdAt').notNull(),
   },
   (table) => {
-    return [primaryKey({ columns: [table.channelID, table.userID] })];
+    return [
+      primaryKey({ columns: [table.channelID, table.userID] }),
+      check(
+        'channelID',
+        sql`octet_length(${table.channelID}) BETWEEN 1 AND ${CHANNEL_ID_MAX_BYTES}`.inlineParams(),
+      ),
+      check(
+        'userID',
+        sql`octet_length(${table.userID}) BETWEEN 1 AND ${USER_ID_MAX_BYTES}`.inlineParams(),
+      ),
+    ];
   },
 );
 
@@ -40,13 +89,36 @@ export const announcementsTable = sqliteTable(
     createdAt: integer('createdAt').notNull(),
   },
   (table) => {
-    return [primaryKey({ columns: [table.channelID, table.announcementID] })];
+    return [
+      primaryKey({ columns: [table.channelID, table.announcementID] }),
+      check(
+        'channelID',
+        sql`octet_length(${table.channelID}) BETWEEN 1 AND ${CHANNEL_ID_MAX_BYTES}`.inlineParams(),
+      ),
+      check(
+        'userID',
+        sql`octet_length(${table.userID}) BETWEEN 1 AND ${USER_ID_MAX_BYTES}`.inlineParams(),
+      ),
+      check(
+        'announcementID',
+        sql`octet_length(${table.announcementID}) BETWEEN 1 AND ${ANNOUNCEMENT_ID_LENGTH}`.inlineParams(),
+      ),
+      check(
+        'headerImage',
+        sql`${table.headerImage} IS NULL OR octet_length(${table.headerImage}) <= ${STORAGE_ID_MAX_BYTES}`.inlineParams(),
+      ),
+      check(
+        'title',
+        sql`${table.title} IS NULL OR octet_length(${table.title}) <= ${ANNOUNCEMENT_TITLE_MAX_BYTES}`.inlineParams(),
+      ),
+      check(
+        'body',
+        sql`octet_length(${table.body}) BETWEEN 1 AND ${ANNOUNCEMENT_BODY_MAX_BYTES}`.inlineParams(),
+      ),
+      check(
+        'images',
+        sql`${table.images} IS NULL OR json_type(${table.images}) = 'array'`.inlineParams(),
+      ),
+    ];
   },
 );
-
-export const blobsTable = sqliteTable('blobs', {
-  blobID: text('blobID').notNull().primaryKey(),
-  contentType: text('contentType').notNull(),
-  data: blob('blob', { mode: 'buffer' }).notNull(),
-  createdAt: integer('createdAt').notNull(),
-});
--- packages/db/src/lib/constants.ts
+++ packages/db/src/lib/constants.ts
@@ -1,6 +1,7 @@
 export const USER_ID_MAX_BYTES = 256;
 export const CHANNEL_ID_MAX_BYTES = 8;
 
+/** @deprecated */
 export const BLOB_ID_MAX_BYTES = 100;
 
 export const CHANNEL_NAME_MAX_BYTES = 150;
@@ -8,8 +9,13 @@ export const CHANNEL_DESC_MAX_BYTES = 1000;
 export const CHANNEL_ICON_MAX_SIZE = 256;
 export const CHANNEL_ICON_MAX_BYTES = 1000000;
 
+/** @deprecated */
 export const ANNOUNCEMENT_ID_SIZE = 6;
+
+export const ANNOUNCEMENT_ID_LENGTH = 6;
 export const ANNOUNCEMENT_TITLE_MAX_BYTES = 200;
 export const ANNOUNCEMENT_BODY_MAX_BYTES = 2000;
 export const ANNOUNCEMENT_IMAGE_MAX_SIZE = 1200;
 export const ANNOUNCEMENT_IMAGE_MAX_BYTES = 2000000;
+
+export const STORAGE_ID_MAX_BYTES = 100;
