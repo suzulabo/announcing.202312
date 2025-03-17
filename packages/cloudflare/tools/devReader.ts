9f1530006ebc00acc1e7cd5b847e56526d0b6f5a
kenji suzuki <41730006+suzulabo@users.noreply.github.com>
2025-03-17 10:15:53 +0900

    Remove cloudflare package (#31)

--- packages/cloudflare/tools/devReader.ts
+++
@@ -1,9 +0,0 @@
-import { genToml } from '../lib/genToml';
-import { runCommand } from '../lib/runCommand';
-
-const main = () => {
-  genToml();
-  runCommand('wrangler pages dev ../reader/.svelte-kit/cloudflare');
-};
-
-main();
--- packages/cloudflare/tools/devWriter.ts
+++
@@ -1,9 +0,0 @@
-import { genToml } from '../lib/genToml';
-import { runCommand } from '../lib/runCommand';
-
-const main = () => {
-  genToml();
-  runCommand('wrangler pages dev ../writer/.svelte-kit/cloudflare');
-};
-
-main();
