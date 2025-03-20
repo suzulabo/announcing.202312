e081ca0b3589de83c8d8497d08b59a7a13821ea6
Kenji Suzuki <41730006+suzulabo@users.noreply.github.com>
2025-03-20 11:26:38 +0900

    Remove idbBlob

--- packages/components/src/lib/utils.ts
+++ packages/components/src/lib/utils.ts
@@ -1,4 +1,3 @@
 export { formatDate } from './utils/formatDate';
-export { loadBlob } from './utils/idbBlob';
 export { createSnapshotContext } from './utils/snapshotContext';
 export { toHtml } from './utils/toHtml';
