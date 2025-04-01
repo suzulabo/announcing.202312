c792d9b26b6dbded94fc61b4a72a027daa95c77b
Kenji Suzuki <41730006+suzulabo@users.noreply.github.com>
2025-04-01 10:23:33 +0900

    Clean genDatePrefix

--- packages/db/src/utils/genDatePrefix.ts
+++ packages/db/src/utils/genDatePrefix.ts
@@ -1,19 +1,17 @@
 const BASE_DATE = Date.UTC(2025, 0, 1, 0, 0, 0, 0);
 const DATE_MSEC = 24 * 60 * 60 * 1000;
 const CHARACTERS = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-
-const toBase62 = (num: number) => {
-  let result = '';
-
-  while (num > 0) {
-    result = CHARACTERS[num % 62] + result;
-    num = Math.floor(num / 62);
-  }
-
-  return result;
-};
+const CHARACTERS_LENGTH = CHARACTERS.length;
 
 export const genDatePrefix = (v: number) => {
-  const d = Math.trunc((v - BASE_DATE) / DATE_MSEC);
-  return toBase62(d).padStart(3, '0');
+  let n = Math.trunc((v - BASE_DATE) / DATE_MSEC);
+
+  let s = '';
+
+  while (n > 0) {
+    s = `${CHARACTERS[n % CHARACTERS_LENGTH]}${s}`;
+    n = Math.trunc(n / CHARACTERS_LENGTH);
+  }
+
+  return s.padStart(3, '0');
 };
