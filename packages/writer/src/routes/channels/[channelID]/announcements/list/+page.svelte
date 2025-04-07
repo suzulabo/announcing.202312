6ef957957450ec9c55f213b62ed5c40099805685
Kenji Suzuki <41730006+suzulabo@users.noreply.github.com>
2025-04-07 11:40:27 +0900

    Fix type Error

--- packages/writer/src/routes/channels/[channelID]/announcements/list/+page.svelte
+++ packages/writer/src/routes/channels/[channelID]/announcements/list/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import ChannelView, { type ChannelViewProps } from '@announcing/components/ChannelView.svelte';
+  import ChannelView from '@announcing/components/ChannelView.svelte';
 
   import { page } from '$app/state';
   import { fetchAnnouncement } from '$lib/fetch/fetchAnnouncement';
@@ -7,6 +7,7 @@
   import { createSnapshotContext } from '@announcing/components/utils';
   import { LL } from '@announcing/i18n';
   import type { PageData } from './$types';
+  import type { ComponentProps } from 'svelte';
 
   interface Props {
     data: PageData;
@@ -16,8 +17,7 @@
 
   export const snapshot = createSnapshotContext();
 
-  let channelViewProps = $derived<ChannelViewProps>({
-    channel: data.channel,
+  let channelViewProps = $derived<ComponentProps<typeof ChannelView>>({
     announcementHrefPrefix: page.url.pathname,
     announcementKeys: data.channel.announcementIDs ?? [],
     announcementLoader: (key: string) => {
@@ -40,7 +40,6 @@
     gap: 8px;
     align-items: center;
     padding: 16px 8px;
-    border-bottom: 1px solid var(--color-border-light);
     margin-bottom: 16px;
     .prompt {
       margin: auto;
