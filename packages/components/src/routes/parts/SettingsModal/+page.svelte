ecdba62521e201fb73eb5f6c1dd185156e8001c8
Kenji Suzuki <41730006+suzulabo@users.noreply.github.com>
2025-04-10 17:44:48 +0900

    Add SettingsModal Component

---
+++ packages/components/src/routes/parts/SettingsModal/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import SettingsModal from '$lib/parts/SettingsModal/SettingsModal.svelte';
+  import { type Locales } from '@announcing/i18n';
+
+  let locale = $state<Locales>('ja');
+  let theme = $state('light');
+
+  let settingsModal: SettingsModal;
+</script>
+
+<button
+  onclick={() => {
+    settingsModal.openModal();
+  }}>Show</button
+>
+
+<SettingsModal
+  bind:this={settingsModal}
+  bind:locale
+  bind:theme
+  onSignOut={() => {
+    alert('sign out');
+  }}
+/>
+
+<style lang="scss">
+  button {
+    display: block;
+    margin: auto;
+  }
+</style>
