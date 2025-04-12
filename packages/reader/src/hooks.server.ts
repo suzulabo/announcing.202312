79719e049336cf3319980fb2dea6e56c4393ff42
Kenji Suzuki <41730006+suzulabo@users.noreply.github.com>
2025-04-12 11:02:41 +0900

    Use setDBEnv in reader

--- packages/reader/src/hooks.server.ts
+++ packages/reader/src/hooks.server.ts
@@ -1,14 +1,13 @@
 import { env } from '$env/dynamic/private';
 import { CF } from '$env/static/private';
-import { setStorage } from '@announcing/db';
+import { setDBEnv, setStorage } from '@announcing/db';
 import { createClient } from '@libsql/client';
 import { type Handle } from '@sveltejs/kit';
 import { sequence } from '@sveltejs/kit/hooks';
 
 import { createLibSqlTokenStore } from '@announcing/notification/tokenStores/libsql';
 
-process.env['DB_URL'] = env.DB_URL;
-process.env['DB_AUTH_TOKEN'] = env.DB_AUTH_TOKEN;
+setDBEnv({ url: env.DB_URL, authToken: env.DB_AUTH_TOKEN });
 
 if (!CF) {
   const createLocalStorage = (await import('@announcing/db/LocalStorage')).createLocalStorage;
