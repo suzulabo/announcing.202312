728dc9e140b74c566da670e2456d1b5e14bb0203
Kenji Suzuki <41730006+suzulabo@users.noreply.github.com>
2025-04-14 16:48:36 +0900

    Add controllerchange handler

--- packages/reader/src/hooks.client.ts
+++ packages/reader/src/hooks.client.ts
@@ -10,27 +10,21 @@ type SWMessageData =
       channelID: string;
     };
 
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.addEventListener('message', (event) => {
-    const data = event.data as SWMessageData;
-    switch (data.type) {
-      case 'log':
-        console.log('SW:', ...data.args);
-        break;
-      case 'openChannel': {
-        const url = `${location.origin}/${data.channelID}`;
-        const urlResolved = resolveBrowserSchema(url);
-        location.href = urlResolved;
-        break;
-      }
+navigator.serviceWorker.addEventListener('message', (event) => {
+  const data = event.data as SWMessageData;
+  switch (data.type) {
+    case 'log':
+      console.log('SW:', ...data.args);
+      break;
+    case 'openChannel': {
+      const url = `${location.origin}/${data.channelID}`;
+      const urlResolved = resolveBrowserSchema(url);
+      location.href = urlResolved;
+      break;
     }
-  });
+  }
+});
 
-  navigator.serviceWorker.ready
-    .then((sw) => {
-      sw.active?.postMessage('hello');
-    })
-    .catch(() => {
-      //
-    });
-}
+navigator.serviceWorker.addEventListener('controllerchange', () => {
+  location.reload();
+});
