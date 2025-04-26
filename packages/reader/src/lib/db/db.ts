b16ba56389b33fd5cf2d93a596d68f31a9237aba
Kenji Suzuki <41730006+suzulabo@users.noreply.github.com>
2025-04-27 07:18:42 +0900

    Fix to make db

--- packages/reader/src/lib/db/db.ts
+++ packages/reader/src/lib/db/db.ts
@@ -1,7 +1,7 @@
-import { building } from '$app/environment';
+import { dev } from '$app/environment';
 import { BUCKET_PREFIX } from '$env/static/private';
 import { createDB } from '@announcing/db';
 
-export const db = building
+export const db = !dev
   ? createDB(BUCKET_PREFIX)
   : await (await import('@announcing/db/localDB')).createLocalDB();
