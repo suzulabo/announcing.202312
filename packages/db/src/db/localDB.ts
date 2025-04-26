82311a21d22c32a03a26d2bfd7ff8618920bb392
Kenji Suzuki <41730006+suzulabo@users.noreply.github.com>
2025-04-27 08:02:51 +0900

    Change to use drizzle-orm/d1/migrator

--- packages/db/src/db/localDB.ts
+++ packages/db/src/db/localDB.ts
@@ -1,8 +1,8 @@
 import type { R2Bucket } from '@cloudflare/workers-types';
 import { drizzle } from 'drizzle-orm/d1';
+import { migrate } from 'drizzle-orm/d1/migrator';
 import { Miniflare } from 'miniflare';
-import { readdir, readFile } from 'node:fs/promises';
-import { join, resolve } from 'node:path';
+import { resolve } from 'node:path';
 import { createAPI, createDB, type DBContext } from './db';
 
 const LOCAL_DIR = '../db-local';
@@ -23,27 +23,9 @@ export const createLocalDB = async (memory = false): Promise<ReturnType<typeof c
   const d1 = await mf.getD1Database('d1');
   const r2 = (await mf.getR2Bucket('r2')) as unknown as R2Bucket;
 
-  await d1.exec('CREATE TABLE IF NOT EXISTS __migrations (name TEXT NOT NULL PRIMARY KEY)');
-
-  const migrations = new Set(
-    (await d1.prepare('SELECT * FROM __migrations ORDER BY name').all()).results.map((row) => {
-      return row['name'] as string;
-    }),
-  );
-
-  const files = await readdir(DRRIZLE_DIR);
-  const sqlFiles = files.filter((file) => file.endsWith('.sql')).sort();
-
-  for (const file of sqlFiles) {
-    if (migrations.has(file)) {
-      continue;
-    }
-    const sqlFile = join(DRRIZLE_DIR, file);
-    const sql = await readFile(sqlFile, 'utf-8');
-    await d1.batch([d1.prepare(sql), d1.prepare('INSERT INTO __migrations VALUES(?)').bind(file)]);
-  }
-
   const db = drizzle(d1);
+
+  await migrate(db, { migrationsFolder: DRRIZLE_DIR });
 
   const makeContext = (): DBContext => {
     return {
