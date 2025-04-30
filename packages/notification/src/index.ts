ba9a245960b7376b9daa661989e0c78d8d1b0661
Kenji Suzuki <41730006+suzulabo@users.noreply.github.com>
2025-04-30 14:26:00 +0900

    Fix build error

--- packages/notification/src/index.ts
+++ packages/notification/src/index.ts
@@ -1,3 +1,2 @@
 export { createDB, type CFBindings as NotificationDBBindings } from './db/db';
-export { createLocalBindings } from './local/localBindings';
 export type { WorkerEnv as NotificationBindings, ProcessMessageParams } from './workflows/types';
