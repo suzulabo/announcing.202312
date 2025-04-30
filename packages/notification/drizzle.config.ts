b45d9f0c7821fc802be0c96bb24604879ba8e2de
Kenji Suzuki <41730006+suzulabo@users.noreply.github.com>
2025-04-30 18:03:51 +0900

    Add configure .env.drizzle.local

--- packages/notification/drizzle.config.ts
+++ packages/notification/drizzle.config.ts
@@ -1,4 +1,11 @@
+import dotenv from 'dotenv';
 import { defineConfig } from 'drizzle-kit';
+
+const env = dotenv.config({ path: '.env.drizzle.local' }).parsed as {
+  CF_ACCOUNT_ID: string;
+  CF_DATABASE_ID: string;
+  CF_D1_TOKEN: string;
+};
 
 // https://orm.drizzle.team/docs/guides/d1-http-with-drizzle-kit
 export default defineConfig({
@@ -7,8 +14,8 @@ export default defineConfig({
   dialect: 'sqlite',
   driver: 'd1-http',
   dbCredentials: {
-    accountId: process.env['CF_ACCOUNT_ID'] ?? '',
-    databaseId: process.env['CF_DATABASE_ID'] ?? '',
-    token: process.env['CF_D1_TOKEN'] ?? '',
+    accountId: env.CF_ACCOUNT_ID,
+    databaseId: env.CF_DATABASE_ID,
+    token: env.CF_D1_TOKEN,
   },
 });
