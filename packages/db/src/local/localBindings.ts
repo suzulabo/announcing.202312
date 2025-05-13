a0603c9561a5a044710e68c3dfb252c994d9f4ef
Kenji Suzuki <41730006+suzulabo@users.noreply.github.com>
2025-05-13 15:19:02 +0900

    Update migrations directory

--- packages/db/src/local/localBindings.ts
+++ packages/db/src/local/localBindings.ts
@@ -5,7 +5,7 @@ import { Miniflare } from 'miniflare';
 import { resolve } from 'node:path';
 
 const LOCAL_DIR = '../db-local';
-const DRRIZLE_DIR = '../db/drizzle';
+const MIGRATIONS_DIR = '../db/migrations';
 
 export const createLocalBindings = async (
   memory = false,
@@ -26,7 +26,7 @@ export const createLocalBindings = async (
 
   const db = drizzle(D1);
 
-  await migrate(db, { migrationsFolder: DRRIZLE_DIR });
+  await migrate(db, { migrationsFolder: MIGRATIONS_DIR });
 
   return { D1, R2 };
 };
