cd57517bfad88c98c832fa7decc793a18ced65d4
Kenji Suzuki <41730006+suzulabo@users.noreply.github.com>
2025-05-25 11:25:28 +0900

    Fix test

--- packages/db/vitest.config.ts
+++ packages/db/vitest.config.ts
@@ -1,9 +1,7 @@
-import dotenv from 'dotenv';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
     include: ['tests/**/*.test.ts'],
-    env: dotenv.config({ path: '.env.test' }).parsed ?? {},
   },
 });
