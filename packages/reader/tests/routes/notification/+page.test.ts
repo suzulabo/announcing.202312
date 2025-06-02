ec2424a82b2b84ee47db9d3badb26900f7b23c13
Kenji Suzuki <41730006+suzulabo@users.noreply.github.com>
2025-06-02 18:40:14 +0900

    Add unit test

---
+++ packages/reader/tests/routes/notification/+page.test.ts
@@ -0,0 +1,99 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { fetchChannel } from '$lib/fetch/fetchChannel';
+import { getNotificationChannels } from '$lib/platform/localStorage';
+import { beforeEach, describe, expect, it, vi, type Mock } from 'vitest';
+import { load } from '../../../src/routes/notification/[[channelID]]/+page';
+
+vi.mock('$app/environment', () => ({
+  browser: true,
+}));
+
+vi.mock('$lib/fetch/fetchChannel', () => ({
+  fetchChannel: vi.fn(),
+}));
+
+vi.mock('$lib/platform/localStorage', () => ({
+  getNotificationChannels: vi.fn(),
+}));
+
+describe('load function', () => {
+  beforeEach(() => {
+    vi.resetAllMocks();
+  });
+
+  it('should sort channels as expected', async () => {
+    const notificationChannels = {
+      c1: { name: 'Channel 1' },
+      c3: { name: 'Channel 3' },
+    };
+
+    (getNotificationChannels as Mock).mockReturnValue(notificationChannels);
+
+    (fetchChannel as Mock).mockImplementation((channelID: string) => {
+      const data: Record<string, unknown> = {
+        c1: { channelID: 'c1', name: 'Channel 1', updatedAt: 100 },
+        c2: { channelID: 'c2', name: 'Channel 2', updatedAt: 200 },
+        c3: { channelID: 'c3', name: 'Channel 3', updatedAt: 50 },
+      };
+      return data[channelID] ?? null;
+    });
+
+    const params = { channelID: 'c2' };
+
+    const result: any = await load({ params } as any);
+
+    expect(result.channels).toEqual([
+      {
+        channelID: 'c2',
+        name: 'Channel 2',
+        updatedAt: 200,
+        status: 'disabled',
+      },
+      {
+        channelID: 'c3',
+        name: 'Channel 3',
+        updatedAt: 50,
+        status: 'enabled',
+      },
+      {
+        channelID: 'c1',
+        name: 'Channel 1',
+        updatedAt: 100,
+        status: 'enabled',
+      },
+    ]);
+  });
+
+  it('should put deleted channels last', async () => {
+    const notificationChannels = {
+      c1: { name: 'Channel 1' },
+      c2: { name: 'Channel 2' },
+    };
+    (getNotificationChannels as Mock).mockReturnValue(notificationChannels);
+
+    (fetchChannel as Mock).mockImplementation((channelID: string) => {
+      if (channelID === 'c1') {
+        return { channelID: 'c1', name: 'Channel 1', updatedAt: 100 };
+      }
+      return null;
+    });
+
+    const params = { channelID: 'cX' };
+
+    const result: any = await load({ params } as any);
+
+    expect(result.channels).toEqual([
+      {
+        channelID: 'c1',
+        name: 'Channel 1',
+        updatedAt: 100,
+        status: 'enabled',
+      },
+      {
+        channelID: 'c2',
+        name: 'Channel 2',
+        status: 'deleted',
+      },
+    ]);
+  });
+});
