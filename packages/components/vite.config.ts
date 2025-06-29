a8830a15e5a16c027e893071a9ccf8505888a044
Kenji Suzuki <41730006+suzulabo@users.noreply.github.com>
2025-06-29 14:12:30 +0900

    Update packages

--- packages/components/vite.config.ts
+++ packages/components/vite.config.ts
@@ -9,12 +9,4 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
   },
-  // https://sass-lang.com/documentation/breaking-changes/legacy-js-api/
-  css: {
-    preprocessorOptions: {
-      scss: {
-        api: 'modern-compiler',
-      },
-    },
-  },
 });
