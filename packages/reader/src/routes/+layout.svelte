4e64a1ccf630fd6c0daa489837b8d04559583a8c
Kenji Suzuki <41730006+suzulabo@users.noreply.github.com>
2025-06-30 11:40:41 +0900

    Add menu bar

--- packages/reader/src/routes/+layout.svelte
+++ packages/reader/src/routes/+layout.svelte
@@ -2,6 +2,7 @@
   import '../app.scss';
 
   import { browser } from '$app/environment';
+  import { afterNavigate } from '$app/navigation';
   import { navigating, page } from '$app/state';
   import MaterialSymbolsNotificationImportantOutlineRounded from '$lib/components/icon/MaterialSymbolsNotificationImportantOutlineRounded.svelte';
   import MaterialSymbolsNotificationsOutlineRounded from '$lib/components/icon/MaterialSymbolsNotificationsOutlineRounded.svelte';
@@ -13,7 +14,7 @@
   import Navigating from '@announcing/components/Navigating.svelte';
   import SettingsModal from '@announcing/components/SettingsModal.svelte';
   import { LL } from '@announcing/i18n';
-  import { onMount, type Snippet } from 'svelte';
+  import { onMount, tick, type Snippet } from 'svelte';
   import { fade } from 'svelte/transition';
   import type { LayoutData } from './$types';
 
@@ -42,14 +43,54 @@
     return 'disabled';
   });
 
+  let menubarHidden = $state(false);
+  let previousScrollY = $state(-1);
+
   let settingsModal: SettingsModal;
 
   onMount(() => {
     document.documentElement.setAttribute('hydrated', '');
   });
 
+  afterNavigate(() => {
+    tick()
+      .then(() => {
+        /**
+         VirtualScrollList used in ChanelView restores scrollY, so reset it.
+        */
+        previousScrollY = -1;
+        menubarHidden = false;
+      })
+      .catch(() => {
+        //
+      });
+  });
+
   setupBack();
 </script>
+
+<svelte:window
+  onscroll={() => {
+    if (previousScrollY < 0) {
+      previousScrollY = window.screenY;
+      return;
+    }
+
+    const currentY = window.scrollY;
+
+    /**
+     Hide the menubar when scrolling down, and restore it when scrolling up.
+     Make it slightly insensitive when hiding.
+    */
+    if (currentY > previousScrollY && currentY - previousScrollY > 10) {
+      menubarHidden = true;
+    } else if (currentY < previousScrollY && previousScrollY - currentY) {
+      menubarHidden = false;
+    }
+
+    previousScrollY = currentY;
+  }}
+/>
 
 <header>
   <button
@@ -95,6 +136,11 @@
     <span class="copyright">&copy;Announcing</span>
   </div>
 </footer>
+
+<div class="menu-bar" class:hidden={menubarHidden}>
+  <button class="unstyled">履歴</button>
+  <button class="unstyled">設定</button>
+</div>
 
 <SettingsModal
   bind:this={settingsModal}
@@ -149,6 +195,24 @@
       .copyright {
         margin: auto;
       }
+    }
+  }
+
+  .menu-bar {
+    margin-top: auto;
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    height: 50px;
+    background-color: var(--color-background);
+    border-top: 1px solid var(--color-border);
+    transition: transform 0.3s ease;
+
+    display: flex;
+
+    &.hidden {
+      transform: translateY(100%);
     }
   }
 </style>
